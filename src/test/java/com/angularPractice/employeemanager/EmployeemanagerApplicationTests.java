192b678a0947a7a4cad708d154469a3302c1033c
Vidit Naithani <102232954+ViditNaithani22@users.noreply.github.com>
2022-12-23 13:02:33 +0530

    initializing backend

---
+++ src/test/java/com/angularPractice/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularPractice.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
