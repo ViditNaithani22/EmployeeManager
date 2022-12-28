7e0c73112cdf96f0e9e62ccdebde0838e5f0609f
Vidit Naithani <102232954+ViditNaithani22@users.noreply.github.com>
2022-12-28 12:40:10 +0530

    controller complete

--- src/main/java/com/angularPractice/employeemanager/EmployeeResource.java
+++ src/main/java/com/angularPractice/employeemanager/EmployeeResource.java
@@ -1,9 +1,13 @@
 package com.angularPractice.employeemanager;
 
 
+import com.angularPractice.employeemanager.model.Employee;
 import com.angularPractice.employeemanager.service.EmployeeService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
@@ -15,5 +19,34 @@ public class EmployeeResource {
         this.employeeService = employeeService;
     }
 
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        List<Employee> employees = employeeService.findAllEmployee();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id){
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee employee1 = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(employee1, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        Employee employee1 = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(employee1, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id){
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
